547d2a2ca9626f9fb764ebfda2aac6eac9dd17cc
Kalouris Gerasimos <kalourakos@hotmail.com>
2018-11-08 15:37:34 +0200

    Update uartTest.py

--- uartTest.py
+++ uartTest.py
@@ -1,1 +1,19 @@
 
+import time
+from Spanner import Spanner
+from Testboard import Testboard
+
+testboard = Testboard("Tester1")
+
+
+if __name__ == "__main__":
+
+    Serial = Testboard.Serial
+
+    my_procedure = testboard.createProcedure('UART').\
+        setup(9600, Serial.DATA_BITS_8 | Serial.STOP_BITS_1 | Serial.PARITY_NO).\
+        doAssertSerialRead('Hello Testboard\n').\
+        doWait(1000).\
+        doSerialWrite('Hello Device\n')
+
+    my_procedure.execute()

0177ad26c7581cec16428c2d4bfec5ab872dc029
Kalouris Gerasimos <kalourakos@hotmail.com>
2018-11-15 14:39:34 +0200

    Update uartTest.py

--- uartTest.py
+++ uartTest.py
@@ -5,15 +5,25 @@ from Testboard import Testboard
 
 testboard = Testboard("testboard_name")
 
+
+def read_in_chunks(file_object, chunk_size):
+    while True:
+        data = file_object.read(chunk_size)
+        if not data:
+            break
+        yield data
+
+
 def myMockTest():
     
     Serial = Testboard.Serial
     my_procedure = testboard.createProcedure('UART').\
-        setup(9600, Serial.DATA_BITS_8 | Serial.STOP_BITS_1 | Serial.PARITY_NO).\
-        doAssertSerialRead('Hello Testboard\n').\
-        doWait(1000).\
-        doSerialWrite('Hello Device\n')
+        setup(9600, Serial.DATA_BITS_8 | Serial.STOP_BITS_1 | Serial.PARITY_NO)
     
+    with open("spannerci.bmp", "r+") as f:
+        for piece in read_in_chunks(f, my_procedure.MAX_PAYLOAD_LEN):
+            my_procedure = my_procedure.doSerialWrite(piece)
+   
     spanner.assertEqual(my_procedure.run(), 0)
 
 if __name__ == "__main__":

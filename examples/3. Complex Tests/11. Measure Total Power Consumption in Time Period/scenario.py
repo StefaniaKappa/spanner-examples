00a6107058c2aa1a650eea17b95e73f458522507
Kalouris Gerasimos <kalourakos@hotmail.com>
2018-11-27 14:32:38 +0200

    Update scenario.py

--- examples/3. Complex Tests/11. Measure Total Power Consumption in Time Period/scenario.py
+++ examples/3. Complex Tests/11. Measure Total Power Consumption in Time Period/scenario.py
@@ -34,25 +34,11 @@ OUTPUT_PIN = "D3"
 
 def measure_power_consumption():
 
-    # Turn the device off
-    testboard.digitalWrite(OUTPUT_PIN, "LOW");
-
-    # Wait for a while for it to shut down
-    time.sleep(10)
-
-    # Turn the device back on
-    testboard.digitalWrite(OUTPUT_PIN, "HIGH");
-
-    # The device runs some initialization actions in the beginning, which are
-    # not indicative of the true power consumption. Therefore we wait for a
-    # while for the initial conditions to pass
-    time.sleep(90)
-
     # Start measuring power consumption
     testboard.startPowerMeasurement()
 
     # Measure for 5 minutes
-    time.sleep(5*60)
+    time.sleep(30)
 
     # Stop measuring power consumption
     testboard.stopPowerMeasurement()

5d476282db477c3855bc0e8b0c8aa5ec3b2794d4
Kalouris Gerasimos <kalourakos@hotmail.com>
2019-01-17 12:54:59 +0200

    Update long_test.py

--- examples/long_test.py
+++ examples/long_test.py
@@ -5,5 +5,5 @@ import time
 
 for i in range(100):
   print(str(i)+'abcdefghijklmnopqrstuvwxyz\n')
-  print('A*dfe34543253455484824432044840548230234404894gfsdf40dsf4848943214e34$#%%^&*&^%$%^&*VBNBVFCDVGBHNJZ)
+  print('A*dfe34543253455484824432044840548230234404894gfsdf40dsf4848943214e34$#%%^&*&^%$%^&*VBNBVFCDVGBHNJZ")
   time.sleep(1)

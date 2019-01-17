0c72ffe9a84643dc2bceb413638f177ceefa6451
Kalouris Gerasimos <kalourakos@hotmail.com>
2019-01-17 15:28:42 +0200

    Update long_test.py

--- examples/long_test.py
+++ examples/long_test.py
@@ -3,6 +3,13 @@
 
 import time
 
-for i in range(100):
-  print('----' + ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(i)) + '----', flush=True)
+for i in range(1, 10):
+  print('----' + ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(i*1000)) + '----', flush=True)
   time.sleep(1)
+
+print("success")
+print("successfully")
+print("Failure")
+print("This is success!")
+print("This is successfully")
+print("success done")

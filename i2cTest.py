30140f4abd64326f0fcc6ebecd026229caa43d1f
Kalouris Gerasimos <kalourakos@hotmail.com>
2019-01-30 13:13:20 +0200

    Update i2cTest.py

--- i2cTest.py
+++ i2cTest.py
@@ -3,7 +3,7 @@ from Testboard import Testboard
 
 testboard = Testboard("testboard_name")
 
-def char_count_mock():
+def z_axis_check():
   my_procedure = testboard.createProcedure('I2C')\
       .setSpeed(100000)\
       .begin()
@@ -17,15 +17,12 @@ def char_count_mock():
   # Execute the mock function
   exit_code, results = my_procedure.run(withResults=True)
   
-  threshold = 100
   for result in results:
     x = int.from_bytes(result[0:2], byteorder='little', signed=True)
     y = int.from_bytes(result[2:4], byteorder='little', signed=True)
     z = int.from_bytes(result[4:6], byteorder='little', signed=True)
     print("X: %.2f, Y: %.2f, Z: %.2f" % (x, y, z))
-    Spanner.assertLessThan(threshold, abs(z - 8000))
-    
+    Spanner.assertLessThan(100, abs(z - 8000))
 
 if __name__ == "__main__":
-  # Start the test
-  char_count_mock()
+  z_axis_check()

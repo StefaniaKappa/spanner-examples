bdfee98f122c9dd79e67104253f3ee3588a1d215
Kalouris Gerasimos <kalourakos@hotmail.com>
2019-01-30 12:03:14 +0200

    Update i2cTest.py

--- i2cTest.py
+++ i2cTest.py
@@ -24,7 +24,7 @@ def char_count_mock():
     y = result[2] | result[3] << 8
     z = result[4] | result[5] << 8
     print(x, y, z)
-    x, y, z = tupple(map(lambda b: int.from_bytes(b, byteorder=sys.byteorder, signed=False), [x, y, z])))
+    x, y, z = tuple(map(lambda b: int.from_bytes(b, byteorder=sys.byteorder, signed=False), [x, y, z])))
     print(x, y, z)
 
 if __name__ == "__main__":

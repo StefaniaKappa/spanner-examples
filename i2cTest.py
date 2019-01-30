ca20e428df8801d5290836d6a116ac59f0fad145
Kalouris Gerasimos <kalourakos@hotmail.com>
2019-01-30 13:06:50 +0200

    Update i2cTest.py

--- i2cTest.py
+++ i2cTest.py
@@ -8,24 +8,22 @@ def char_count_mock():
       .setSpeed(100000)\
       .begin()
   
-  for _ in range(10):
+  for _ in range(3):
       my_procedure\
         .write(0x18, bytearray([168]))\
         .read(0x18, 6)\
-        .doWait(200)
+        .doWait(1000)
 
   # Execute the mock function
   exit_code, results = my_procedure.run(withResults=True)
   
+  threshold = 100
   for result in results:
     x = int.from_bytes(result[0:2], byteorder='little', signed=True)
     y = int.from_bytes(result[2:4], byteorder='little', signed=True)
     z = int.from_bytes(result[4:6], byteorder='little', signed=True)
-    #X_g = x / 8190.0
-    #Y_g = y / 8190.0
-    #Z_g = z / 8190.0
-    #print("X: %.2f, Y: %.2f, Z: %.2f" % (X_g, Y_g, Z_g))
     print("X: %.2f, Y: %.2f, Z: %.2f" % (x, y, z))
+    Spanner.assertLessThan(threshold, abs(z - 8000))
     
 
 if __name__ == "__main__":

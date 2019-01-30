de525e9f8604bbc4cae0e8c878816dc95f97f7eb
Kalouris Gerasimos <kalourakos@hotmail.com>
2019-01-30 10:54:42 +0200

    Update i2cTest.py

--- i2cTest.py
+++ i2cTest.py
@@ -7,14 +7,17 @@ testboard = Testboard("testboard_name")
 def char_count_mock():
   my_procedure = testboard.createProcedure('I2C')\
       .setSpeed(100000)\
-      .begin()\
-      .write(0x18, bytearray([168]))\
-      .read(0x18, 6)\
-      .write(0x18, bytearray([168]))\
-      .read(0x18, 6)
+      .begin()
+  
+  for _ in range(10):
+      my_procedure\
+        .write(0x18, bytearray([168]))\
+        .read(0x18, 6)
 
   # Execute the mock function
-  my_procedure.run(withResults=True)
+  exit_code, results = my_procedure.run(withResults=True)
+  
+  print(results)
 
 if __name__ == "__main__":
   # Start the test

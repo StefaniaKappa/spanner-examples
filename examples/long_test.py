7e06a9f89806f2a7c879ad741005b75f5f433db1
Kalouris Gerasimos <kalourakos@hotmail.com>
2019-02-13 10:29:06 +0200

    Update long_test.py

--- examples/long_test.py
+++ examples/long_test.py
@@ -5,9 +5,9 @@ import time
 import string
 import random
 
-for i in range(1, 10):
-  print('----' + ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(i*1000)) + '----', flush=True)
-  time.sleep(100)
+# for i in range(1, 10):
+#   print('----' + ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(i*1000)) + '----', flush=True)
+#   time.sleep(100)
 
 print("success")
 print("successfully")
